12427e7c93e08b8fdf4f4cdfbe3b27d8869d991d
vsokolosky <113938464+vsokolosky@users.noreply.github.com>
2023-02-13 22:28:20 -0700

    Update ex1.5.py
    fixed error where storage wasn't being cleared between tests

--- ex1.5.py
+++ ex1.5.py
@@ -26,10 +26,11 @@ def func(n):
         storage[n] = func(n-1) + func(n-2)
     return storage[n]
 
-r = range(0, 36)
-timer_data = []
-for i in r:
-    elapsed_time = timeit.timeit(lambda : func(i), number=1)
-    timer_data.append(elapsed_time)
-plt.plot(r, timer_data)
+r2 = range(0, 36)
+timer_data2 = []
+for i in r2:
+    elapsed_time2 = timeit.timeit(lambda : func(i), number=1)
+    timer_data2.append(elapsed_time2)
+    storage.clear()
+plt.plot(r2, timer_data2)
 plt.show()
